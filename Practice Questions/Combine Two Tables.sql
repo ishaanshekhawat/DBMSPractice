e746b6a6545eee36a3b02dcec88047a24e521c8a
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-21 23:49:06 +0530

    Create Combine Two Tables.sql

---
+++ Practice Questions/Combine Two Tables.sql
@@ -0,0 +1,9 @@
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+
+-- Return the result table in any order.
+
+select firstName, lastName, city, state
+from Person 
+left join Address
+on Person.personId = Address.personId
