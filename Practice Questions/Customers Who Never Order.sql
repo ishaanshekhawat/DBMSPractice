956213a225c3c8946f30bf3cbdd0cdf77479f78b
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-22 23:09:17 +0530

    Create Customers Who Never Order.sql

---
+++ Practice Questions/Customers Who Never Order.sql
@@ -0,0 +1,35 @@
+-- Write a solution to find all customers who never order anything.
+
+-- Return the result table in any order.
+
+-- Example 1:
+-- Input: 
+-- Customers table:
+-- +----+-------+
+-- | id | name  |
+-- +----+-------+
+-- | 1  | Joe   |
+-- | 2  | Henry |
+-- | 3  | Sam   |
+-- | 4  | Max   |
+-- +----+-------+
+-- Orders table:
+-- +----+------------+
+-- | id | customerId |
+-- +----+------------+
+-- | 1  | 3          |
+-- | 2  | 1          |
+-- +----+------------+
+-- Output: 
+-- +-----------+
+-- | Customers |
+-- +-----------+
+-- | Henry     |
+-- | Max       |
+-- +-----------+
+
+select name as Customers
+from Customers c
+left join Orders o
+on c.id = o.customerId
+where o.customerId is null
