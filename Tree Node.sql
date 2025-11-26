f4b00380e8b12933c8fdd194f0108fb2fffa1d1e
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-26 21:43:33 +0530

    Create Tree Node.sql

---
+++ Tree Node.sql
@@ -0,0 +1,46 @@
+-- Each node in the tree can be one of three types:
+
+-- "Leaf": if the node is a leaf node.
+-- "Root": if the node is the root of the tree.
+-- "Inner": If the node is neither a leaf node nor a root node.
+-- Write a solution to report the type of each node in the tree.
+
+-- Return the result table in any order.
+
+-- Input: 
+-- Tree table:
+-- +----+------+
+-- | id | p_id |
+-- +----+------+
+-- | 1  | null |
+-- | 2  | 1    |
+-- | 3  | 1    |
+-- | 4  | 2    |
+-- | 5  | 2    |
+-- +----+------+
+-- Output: 
+-- +----+-------+
+-- | id | type  |
+-- +----+-------+
+-- | 1  | Root  |
+-- | 2  | Inner |
+-- | 3  | Leaf  |
+-- | 4  | Leaf  |
+-- | 5  | Leaf  |
+-- +----+-------+
+-- Explanation: 
+-- Node 1 is the root node because its parent node is null and it has child nodes 2 and 3.
+-- Node 2 is an inner node because it has parent node 1 and child node 4 and 5.
+-- Nodes 3, 4, and 5 are leaf nodes because they have parent nodes and they do not have child nodes.
+
+SELECT id, 
+CASE
+    WHEN p_id IS NULL THEN 'Root'
+    WHEN id IN (
+        SELECT distinct(p_id)
+        FROM Tree
+        WHERE p_id IS NOT NULL
+    ) THEN 'Inner'
+    ELSE 'Leaf'
+    END AS type
+FROM Tree
