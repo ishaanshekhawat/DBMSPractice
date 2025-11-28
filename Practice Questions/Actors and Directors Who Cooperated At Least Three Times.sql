a232884d66d031389ef50660082255713cc6870f
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2025-11-28 18:10:09 +0530

    Create Actors and Directors Who Cooperated At Least Three Times.sql

---
+++ Practice Questions/Actors and Directors Who Cooperated At Least Three Times.sql
@@ -0,0 +1,33 @@
+-- Write a solution to find all the pairs (actor_id, director_id) where the actor has cooperated with the director at least three times.
+-- Return the result table in any order.
+-- The result format is in the following example.
+
+-- Example 1:
+-- Input: 
+-- ActorDirector table:
+-- +-------------+-------------+-------------+
+-- | actor_id    | director_id | timestamp   |
+-- +-------------+-------------+-------------+
+-- | 1           | 1           | 0           |
+-- | 1           | 1           | 1           |
+-- | 1           | 1           | 2           |
+-- | 1           | 2           | 3           |
+-- | 1           | 2           | 4           |
+-- | 2           | 1           | 5           |
+-- | 2           | 1           | 6           |
+-- +-------------+-------------+-------------+
+-- Output: 
+-- +-------------+-------------+
+-- | actor_id    | director_id |
+-- +-------------+-------------+
+-- | 1           | 1           |
+-- +-------------+-------------+
+-- Explanation: The only pair is (1, 1) where they cooperated exactly 3 times.
+
+SELECT actor_id, director_id
+FROM (
+    SELECT actor_id, director_id, COUNT(*) AS num
+    FROM ActorDirector
+    GROUP BY actor_id, director_id
+) AS table1
+WHERE table1.num > 2
