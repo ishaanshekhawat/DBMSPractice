3788614dc6e4a13ea2f1cf9720a87a2d9908d6c1
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-01-08 14:20:33 +0530

    Create Duplicate Job Listings - 2nd try.sql

---
+++ Practice Questions/Duplicate Job Listings - 2nd try.sql
@@ -0,0 +1,8 @@
+-- https://datalemur.com/questions/duplicate-job-listings
+
+select count(*) as duplicate_companies
+from (
+  select *, count(*) as cnt
+  from job_listings
+  group by company_id, title, description) as temp
+where cnt > 1;
