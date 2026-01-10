08a4569fd8275ab67997e29e6ddcd1a37894d9e6
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-01-11 01:13:30 +0530

    Create Pharmacy Analytics (Part 3) - 2nd Try.sql

---
+++ Practice Questions/Pharmacy Analytics (Part 3) - 2nd Try.sql
@@ -0,0 +1,6 @@
+-- https://datalemur.com/questions/total-drugs-sales
+
+select manufacturer, concat('$',round(sum(total_sales)/1000000, 0),' million') as sale
+from pharmacy_sales
+group by manufacturer
+order by sum(total_sales) desc, manufacturer
