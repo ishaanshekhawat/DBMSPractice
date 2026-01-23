4ffb773e1b1b135e0a6a90e48005e6f27331c004
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-01-23 23:27:47 +0530

    Create Find Users with High Token Usage.sql

---
+++ Practice Questions/Find Users with High Token Usage.sql
@@ -0,0 +1,7 @@
+-- https://leetcode.com/problems/find-users-with-high-token-usage/
+
+select user_id, count(prompt) as prompt_count, round(avg(tokens),2) as avg_tokens
+from prompts
+group by user_id
+having count(prompt) >= 3 and max(tokens) > avg(tokens)
+order by avg_tokens desc, user_id asc
