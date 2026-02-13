b6006e2494dd99cd85401ec7384f96c4e3f7e23e
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-02-14 03:10:46 +0530

    Create Employees Whose Manager Left the Company.sql

---
+++ Practice Questions/Employees Whose Manager Left the Company.sql
@@ -0,0 +1,8 @@
+-- https://leetcode.com/problems/employees-whose-manager-left-the-company/
+
+select employee_id
+from Employees e1
+where salary < 30000 and manager_id not in (
+    select employee_id from Employees e2
+    )
+order by employee_id
