b3b865a6a3d91ed275d41e84977096aaded9b901
Ishaan Singh Shekhawat <ishaan.singh.shekhawat@gmail.com>
2026-03-16 16:44:03 +0530

    Create Order Details.sql

---
+++ Practice Questions/Order Details.sql
@@ -0,0 +1,5 @@
+-- https://platform.stratascratch.com/coding/9913-order-details
+
+select first_name, order_date, order_details, total_order_cost
+from customers c join orders o on c.id = o.cust_id
+where first_name = 'Jill' or first_name = 'Eva';
